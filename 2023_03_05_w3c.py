a1bc37703cda14366ec15a8b96de8b451740b71b
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-03-05 12:50:04 +0100

    Commit nr 003 - selenium, sterowanie stroną z poziomu kodu

---
+++ 2023_03_05_w3c.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+import time
+
+#Jeśli import z selenium nie zadziała to trzeba w terminalu
+#wpisać pip install selenium
+
+driver = webdriver.Chrome()
+driver.get("https://www.w3schools.com/")
+time.sleep(5)
+
+#driver.set_window_size(1200, 1000)
+driver.maximize_window()
+
+accept = driver.find_element("id", "accept-choices")
+accept.click()
+
+menu = driver.find_element("id", "navbtn_tutorials")
+#Tutaj dopiero najeżdżamy na "Tutorials"
+webdriver.ActionChains(driver).move_to_element(menu).perform()
+#A tutaj klikamy
+webdriver.ActionChains(driver).move_to_element(menu).click().perform()
+
+HTMLtutu = driver.find_element('xpath', '//*[@id="nav_tutorials"]/div/div/div[2]/a[1]')
+HTMLtutu.click()
+# HTMLAtributs = driver.find_element('xpath', '//*[@id="leftmenuinnerinner"]/a[64]')
+# HTMLAtributs.click()
+
+driver.find_element('xpath', '//*[@id="leftmenuinnerinner"]/a[64]').click()
+driver.find_element('xpath', '//*[@id="main"]/div[3]/table/tbody/tr[32]/td[1]/a').click()
+driver.find_element('xpath', '//*[@id="main"]/div[5]/a').click()
+
+print(driver.title)
+
+currentWindowName = driver.current_window_handle
+windowNames = driver.window_handles
+
+for window in windowNames:
+    if window != currentWindowName:
+        driver.switch_to.window(window)
+
+print(driver.title)
+driver.find_element('xpath', '//*[@id="runbtn"]').click()
+
+time.sleep(500)
+driver.close()
+
+
+
+
